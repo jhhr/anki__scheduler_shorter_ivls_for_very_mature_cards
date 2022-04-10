a739518abb87a3b93f7d2fde092a7fbf0ad61427
Joonas Rajamäki <joonas.rajamaki@inforglobe.com>
2022-04-10 18:20:48 +0300

    Adjust growth rate based on card rep count also

--- src/reduce_Interval_growth_for_very_mature_cards.py
+++ src/reduce_Interval_growth_for_very_mature_cards.py
@@ -4,17 +4,17 @@ import sys
 import math
 
 from anki.cards import Card
-from anki.scheduler.v1 import Scheduler as oldsched
-from anki.scheduler.v2 import Scheduler as v2sched
+from anki.scheduler.v2 import Scheduler as V2Scheduler
 
 from aqt import mw
-from aqt.utils import showInfo
-from anki.hooks import wrap
 
 from .config import gc
 
+def get_all_reps(card=mw.reviewer.card):
+    return mw.col.db.list("select ease from revlog where cid = ? and "
+                          "type IN (0, 1, 2, 3)", card.id)
 
-def _modify_ivl_for_very_mature_cards(self, prelim_new_ivl, conf, fct, mult):
+def _modify_ivl_for_very_mature_cards(self, prelim_new_ivl, conf, fct, mult, rep_count):
     """all inputs are integers and fractions of integers are rounded down"""
     red = gc("don't reduce ivls if they are already capped by the deck maxIvl")
 
@@ -26,6 +26,18 @@ def _modify_ivl_for_very_mature_cards(self, prelim_new_ivl, conf, fct, mult):
     # Higher ease = longer change to logarithmic growth = lower review frequency
     # This keeps high ease cards different from low ease even when they're very mature
     adj_days_upper = float(gc("days_upper")) * fct
+    # Additionally adjust days_upper by review count
+    # The more reviews a card has, the lower days_upper should be
+    rep_count_limit = gc("rep_count_limit")
+    if gc("debug"):
+        print('min_mod_fct', min_mod_fct)
+        print('rep_count / limit', f"{rep_count} / {rep_count_limit}")
+        print('days_upper adj 1', adj_days_upper)
+        print('rep_count', rep_count)
+    if rep_count_limit > 0:
+        adj_days_upper = max(gc("days_lower"), adj_days_upper * (1 - rep_count / rep_count_limit))
+        if gc("debug"):
+            print('days_upper adj 2', adj_days_upper)
 
     if red and prelim_new_ivl > conf["maxIvl"]:
         return prelim_new_ivl
@@ -39,55 +51,9 @@ def _modify_ivl_for_very_mature_cards(self, prelim_new_ivl, conf, fct, mult):
         p = days_over_lower / days_range
         # Return gradually increasing portion of fully modded vs normal ivl
         mod_ivl = int(min(prelim_new_ivl, prelim_new_ivl * (1 - p) + full_mod_ivl * p))
-        print('mod_ivl', prelim_new_ivl, mod_ivl)
+        if gc("debug"):
+            print('mod_ivl', prelim_new_ivl, mod_ivl)
         return mod_ivl
-
-
-# this is a modified version of _nextRevIvl from sched.py
-def nextRevIvlMod__v1(self, card, ease):
-    "Ideal next interval for CARD, given EASE."
-    delay = self._daysLate(card)
-    conf = self._revConf(card)
-    fct = card.factor / 1000.0
-
-    ###start of modification###
-    if card.ivl >= gc("delay: don't modify delay for cards with ivls below"):
-        delay = int(delay * gc("delay: percentage")/100.0)
-
-    if gc("ease_min"):
-        min_fct = gc("ease_min")/100.0
-        if min_fct >= fct:
-            fct = min_fct
-    if gc("ease_max"):
-        max_fct = gc("ease_max")/100.0
-        if max_fct <= fct:
-            fct = max_fct
-    ###end of modification###
-
-    prelim_ivl2 = self._constrainedIvl(
-        (card.ivl + delay // 4) * 1.2, conf, card.ivl)
-    prelim_ivl3 = self._constrainedIvl(
-        (card.ivl + delay // 2) * fct, conf, prelim_ivl2)
-    prelim_ivl4 = self._constrainedIvl(
-        (card.ivl + delay) * fct * conf['ease4'], conf, prelim_ivl3)
-
-    ###start of modification###
-    ivl2 = _modify_ivl_for_very_mature_cards(
-        self, prelim_ivl2, conf, fct, 1)
-    ivl3 = _modify_ivl_for_very_mature_cards(
-        self, prelim_ivl3, conf, fct, 1)
-    ivl4 = _modify_ivl_for_very_mature_cards(
-        self, prelim_ivl4, conf, fct, conf['ease4'])
-    ###end of modification###
-
-    if ease == 2:
-        interval = ivl2
-    elif ease == 3:
-        interval = ivl3
-    elif ease == 4:
-        interval = ivl4
-    # interval capped?
-    return min(interval, conf['maxIvl'])
 
 
 def nextRevIvlMod__v2(self, card: Card, ease: int, fuzz: bool) -> int:
@@ -124,12 +90,14 @@ def nextRevIvlMod__v2(self, card: Card, ease: int, fuzz: bool) -> int:
     prelim_ivl4 = f((card.ivl + delay) * fct *
                     conf["ease4"], conf, prelim_ivl3, fuzz)
 
+    rep_count = len(get_all_reps(card))
+
     ivl2 = _modify_ivl_for_very_mature_cards(
-        self, prelim_ivl2, conf, fct, 1)
+        self, prelim_ivl2, conf, fct, 1, rep_count)
     ivl3 = _modify_ivl_for_very_mature_cards(
-        self, prelim_ivl3, conf, fct, 1)
+        self, prelim_ivl3, conf, fct, 1, rep_count)
     ivl4 = _modify_ivl_for_very_mature_cards(
-        self, prelim_ivl4, conf, fct, conf['ease4'])
+        self, prelim_ivl4, conf, fct, conf['ease4'], rep_count)
     ###end of modification###
 
     if ease == 2:
@@ -138,8 +106,6 @@ def nextRevIvlMod__v2(self, card: Card, ease: int, fuzz: bool) -> int:
         return ivl3
     return ivl4
 
-
-oldsched.original_nextRevIvl = oldsched._nextRevIvl  # for add-on sidebar for scheduler comparison
-oldsched._nextRevIvl = nextRevIvlMod__v1
-v2sched.original_nextRevIvl = v2sched._nextRevIvl
-v2sched._nextRevIvl = nextRevIvlMod__v2
+# for add-on sidebar for scheduler comparison
+V2Scheduler.original_nextRevIvl = V2Scheduler._nextRevIvl
+V2Scheduler._nextRevIvl = nextRevIvlMod__v2
